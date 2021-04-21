34c3df2caf82e557ea589075234d51364cf730d2
Waseem Brare <waseembrare@gmail.com>
2021-04-21 22:31:38 +0100

    added favicon and changed the site title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Waseem Brare'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Waseem Brare | Blog'
 
 export default function Layout({ children, home }) {
     return (
